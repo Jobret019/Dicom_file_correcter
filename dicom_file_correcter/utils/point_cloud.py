0bab4413cc6288d4af16f178e5266b2e2ddf7624
Jobret019 <jonathanbreton095@gmail.com>
2023-07-20 12:01:10 -0400

    add position_patient_coordiante in point_cloud

--- dicom_file_correcter/utils/point_cloud.py
+++ dicom_file_correcter/utils/point_cloud.py
@@ -1,14 +1,12 @@
-import position_patient_coordinate as ppc
-import pydicom 
 import numpy as np
 import os 
-from dicompylercore import dicomparser
+import pydicom
 
 
 def image_point_cloud(path_to_serie) : 
     '''
-    This method create an array of 3 array (x,y,z position) with all the position of the pixels with the 
-    highest radiodensity value which correspond to source position in an image 
+    This method create an array of 3 array (x,y,z position) with all the position in mm of 
+    the pixels with the highest radiodensity value which correspond to source position in an image 
     
     :param path_to_serie : complete path of the serie containing all the CT files
 
@@ -26,7 +24,7 @@ def image_point_cloud(path_to_serie) :
     list_z_position=np.array([])
 
     paths = os.listdir(path_to_serie)
-    pixel_positions_x,pixel_positions_y,_=ppc.pixel_position_in_patient_coordinate(os.path.join(path_to_serie,paths[0]))
+    pixel_positions_x,pixel_positions_y,_=pixel_position_in_patient_coordinate(os.path.join(path_to_serie,paths[0]))
 
     for i in range(len(paths)) : 
         complete_path=os.path.join(path_to_serie,paths[i])
@@ -68,7 +66,7 @@ def source_point_cloud(path_to_plan) :
     aplication_setups=open_plan.ApplicationSetupSequence
 
     for i in range(len(aplication_setups)) :
-        source_position= ppc.source_position_in_patient_coordinate(path_to_plan,i) 
+        source_position= source_position_in_patient_coordinate(path_to_plan,i) 
         if source_position==None : 
             list_source_position+=[]       
         elif len(source_position)==2 :
@@ -96,7 +94,7 @@ def source_point_cloud(path_to_plan) :
 def image_hu_data(path_to_image_file) : 
     '''
     This method create a 2D array with all the value of radiodensity in HU associated with every pixel 
-    of an image 
+    of a CT image 
     
     :param path_to_image_file : complete path of an CT image file
 
@@ -111,3 +109,72 @@ def image_hu_data(path_to_image_file) :
 
     data=pixel_data*rescale_slop+rescale_intercept
     return data
+
+def pixel_position_in_patient_coordinate(path_to_image_file) : 
+    '''
+    This method express the xyz position of the pixels in an image slice in the patient coordinate 
+    system (mm).
+    
+    :param path_to_image_file : complete path of an CT image file
+
+    :return: two arrays of the positions of x and y in mm for all the pixels  and a float for the 
+    position in z 
+    '''
+    open_dicom=pydicom.dcmread(path_to_image_file)
+    orientation=open_dicom.ImageOrientationPatient
+    position=open_dicom.ImagePositionPatient
+    pixel_spacing=open_dicom.PixelSpacing
+    colonne=open_dicom.Columns
+    rangee=open_dicom.Rows
+    
+    Xx,Xy,Xz,Yx,Yy,Yz=orientation[0],orientation[1],orientation[2],orientation[3],orientation[4],orientation[5]
+    Sx,Sy,Sz=position[0],position[1],position[2]
+    deltai,deltaj=pixel_spacing[0],pixel_spacing[1]
+    
+    M=[[Xx*deltai,Yx*deltaj,0,Sx],[Xy*deltai,Yy*deltaj,0,Sy],[Xz*deltai,Yz*deltaj,0,Sz],[0,0,0,1]] 
+    Position=np.empty((colonne,rangee),dtype=object)
+    Position_X=np.empty(colonne,dtype=object)
+    Position_Y=np.empty(rangee,dtype=object)
+    
+    for i in range(colonne) : 
+        for j in range(rangee) : 
+            vecteur = np.array([i,j,0,1]) 
+            Position[i][j]=np.dot(M,vecteur)
+            Position[i][j]=np.delete(Position[i][j],3)
+            Position_Y[j]=Position[0][j][1]
+        Position_X[i]=Position[i][0][0]
+    
+    Position_Z=Position[1][1][2]
+    
+    return Position_X,Position_Y,Position_Z
+
+def source_position_in_patient_coordinate(path_to_plan,setup_number) :
+    '''
+    This method extract the position of a source in a RTPLAN dicom files .
+    
+    :param path_to_plan : complete path of an RTPLAN file
+    :param setup_number : the setup number of the source 
+
+    :return: 1 or 2 array of source xyz position in mm 
+    '''
+    open_plan=pydicom.dcmread(path_to_plan)
+    application_setup=open_plan.ApplicationSetupSequence
+    
+    if hasattr(application_setup[setup_number],'ChannelSequence') : 
+        channel_sequence=application_setup[setup_number].ChannelSequence
+        Control_point=channel_sequence[0].BrachyControlPointSequence
+        if len(Control_point) == 4 : 
+            source_position_2=Control_point[2].ControlPoint3DPosition 
+        elif len(Control_point) == 2 : 
+            source_position_2=None
+
+        source_position_1=Control_point[0].ControlPoint3DPosition 
+        
+        if source_position_2==None : 
+            return source_position_1
+        else : 
+            return source_position_1,source_position_2 
+    
+    else : 
+        pass
+
