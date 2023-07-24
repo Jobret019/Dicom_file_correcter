b91c69558b647ebe93da04d691e7d9d044f62fc2
Jobret019 <jonathanbreton095@gmail.com>
2023-07-24 10:59:17 -0400

    put a global icp instead of two

--- dicom_file_correcter/Iterative_closest_point.py
+++ dicom_file_correcter/Iterative_closest_point.py
@@ -1,5 +1,43 @@
-import numpy as np
+import numpy as np 
+import utils.point_cloud_array_creator as pc 
+import Iterative_closest_point as icp
 from sklearn.neighbors import KDTree
+
+def apply_transformation_on_image_cloud(image_cloud,source_cloud) : 
+    '''
+    This method shift an image point cloud in the correct position in the case of a point
+    cloud inverted relative to the z axis
+
+    :param image_cloud : the image point cloud to be shifted 
+    :param source_cloud : the source point cloud that the image_cloud is gonna be register 
+    on 
+
+    :return : the corrected image point cloud witgh new point position. 
+    '''
+    image_cloud=pc.flip_image_point_cloud(image_cloud) 
+    translation=icp_translation(image_cloud,source_cloud) 
+    I_matrix=np.array([[1,0,0],[0,1,0],[0,0,1]])
+    corrected_image_point_cloud=ApplyTransformation(image_cloud,I_matrix,translation) 
+    return corrected_image_point_cloud 
+
+def icp_translation(image_cloud,source_cloud) : 
+    '''
+    This method find the optimal translation to register two point cloud together 
+    using the ICP algorithm  
+
+    :param path_series0 : the path of series0 of the patient 
+    :param path_RTPALN : The path of the RTPLAN file of the patient
+    :param inverse : if inverse is True, the method will first inverse the image point 
+    cloud in the z axis
+
+    :return : the translation to register the 2 point cloud 
+
+    '''
+    transformation=IterativeClosestPoint(image_cloud,source_cloud) 
+    translation = transformation[1]    
+    return translation
+
+
 
 ''' 
 The following lines were on the module icp.py of the code created by iitaakash
