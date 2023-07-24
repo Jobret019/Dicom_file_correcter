532d19c92b0a49b489b0a3654b8950b35d4ea6e9
Jobret019 <jonathanbreton095@gmail.com>
2023-07-24 11:05:37 -0400

    change typo error on corrector

--- dicom_file_correcter/main.py
+++
@@ -1,4 +0,0 @@
-import file_correcter 
-import utils.point_cloud as pc 
-import apply_icp
-import patient_folder
---
+++ dicom_file_corrector/main.py
@@ -0,0 +1,4 @@
+import file_corrector 
+import utils.point_cloud_array_creator as pc 
+import Iterative_closest_point
+import patient_folder_context_extractor
